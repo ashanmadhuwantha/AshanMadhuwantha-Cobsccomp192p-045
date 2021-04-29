36cff03f259d8295ae8083beb30fab6e34567495
ashanM <ashmadhuwantha@gmail.com>
2021-04-30 01:23:50 +0530

    category table view added

---
+++ AshanMadhuwantha-Cobsccomp192p-045/TableViewCells/CategoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryTableViewCell.swift
+//  AshanMadhuwantha-Cobsccomp192p-045
+//
+//  Created by Ashan Madhuwantha on 2021-04-30.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
