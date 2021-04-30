c17cc02262aa65d8ace70eecb284cb6d6a83a030
ashanM <ashmadhuwantha@gmail.com>
2021-04-30 19:01:45 +0530

    image upload done

--- AshanMadhuwantha-Cobsccomp192p-045/Controller/Store/MenuViewController.swift
+++ AshanMadhuwantha-Cobsccomp192p-045/Controller/Store/MenuViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseStorage
+import Loaf
 
 class MenuViewController: UIViewController {
     
@@ -44,6 +45,18 @@ class MenuViewController: UIViewController {
     }
     
     @IBAction func onAddFoodPressed(_ sender: UIButton) {
+        
+        let foodItem = FoodItem(
+        foodName: txtName.text ?? "",
+        foodId: "",
+        foodDescription: txtDescription.text ?? "",
+        foodPrice: Double(txtPrice.text ?? "") ?? 0,
+        discount: Int(txtDiscount.text ?? "") ?? 0,
+        foodImgRes: "",
+        foodCategory: categoryList[selectedCategoryIndex].categoryName,
+        availability: true)
+        
+        self.addFoodItem(foodItem: foodItem)
     }
     
     @objc func onPickImageClicked(_ sender: UIImageView) {
@@ -63,9 +76,68 @@ class MenuViewController: UIViewController {
 }
 
 extension MenuViewController {
-    func addFoodItem() {
+    
+    func addFoodItem(foodItem: FoodItem) {
         
-    }
+        guard let image = self.selectedImage else {
+                    Loaf("Add an image", state: .error, sender: self).show()
+                    return
+                }
+                
+                if let uploadData = image.jpegData(compressionQuality: 0.5) {
+                    
+                    let metaData = StorageMetadata()
+                    metaData.contentType = "image/jpeg"
+                    
+                    Storage.storage().reference().child("foodItemImages").child(foodItem.foodName).putData(uploadData, metadata: metaData) {
+                        meta, error in
+                        
+                        if let error = error {
+                            print(error.localizedDescription)
+                            Loaf(error.localizedDescription, state: .error, sender: self).show()
+                            return
+                        }
+                        
+                        Storage.storage().reference().child("foodItemImages").child(foodItem.foodName).downloadURL(completion: {
+                            (url,error) in
+                            guard let downloadURL = url else {
+                                if let error = error {
+                                    print(error.localizedDescription)
+                                    Loaf(error.localizedDescription, state: .error, sender: self).show()
+                                }
+                                return
+                            }
+                            
+        Loaf("Image uploaded", state: .success, sender: self).show()
+        
+        let data = [
+            "food_name": foodItem.foodName,
+            "description": foodItem.foodDescription,
+            "price": foodItem.foodPrice,
+            "discount": foodItem.discount,
+            "category": foodItem.foodCategory,
+            "isActive": foodItem.availability,
+            "image": downloadURL.absoluteString
+        
+        ] as [String : Any]
+        
+                            self.databaseReference
+                                                   .child("foodItems")
+                                                   .childByAutoId()
+                                                   .setValue(data) {
+                                                       error, ref in
+                                                       if let error = error {
+                                                           Loaf(error.localizedDescription, state: .error, sender: self).show()
+                                                       } else {
+                                                           Loaf("Food item added", state: .success, sender: self).show()
+                                                       }
+                                                   }
+                                               
+                                           })
+                                       }
+                                   }
+                                   
+                               }
     
     func refreshCategories() {
         self.categoryList.removeAll()
